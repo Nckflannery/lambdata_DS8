6b37f53c90ccc9c742e44eddd4cb37b52bfea93d
Nckflannery <Nckflannery@users.noreply.github.com>
2019-10-28 10:24:05 -0700

    Added init and utils files with test functions

---
+++ lambdata_Nckflannery/__init__.py
@@ -0,0 +1,16 @@
+"""
+lambdata - a collection of data science helper functions
+"""
+import pandas as pd
+import numpy as np
+
+# Sample Code
+
+
+# Sample Datasets
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
+
+# Sample Functions
+def increment(x):
+    return(x+1)
---
+++ lambdata_Nckflannery/df_utils.py
@@ -0,0 +1,5 @@
+'''
+Utility functions for working with dataframes
+'''
+import pandas as pd
+TEST_DF = pd.DataFrame([1,2,3,4,5,6])
